c08e2efd78a477ad7b1c2bce59e805d410f3d7db
Hanleyjames <hanley.doth@outlook.com>
2018-09-07 15:45:20 -0700

    Add temp fixed brew roster, and list

---
+++ src/components/shared/List.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+let fixedBrewRoster = [
+  {
+    name: 'Grandpa Kurtis Ginger Kambucha',
+    brewer: 'Granpappy Kurt',
+    description: 'Probiotic and Prebiotic accessories',
+    abv: '12%',
+    price: '14',
+    remaining: '34'
+  },
+  {
+    name: 'Grandpa Kurtis Lemon and Chili Kambucha',
+    brewer: 'Granpappy Kurt',
+    description: 'Probiotic and Prebiotic accessories',
+    abv: '20%',
+    price: '15',
+    remaining: '20'
+  }
+];
+
+function List(){}
+
+export default List;
